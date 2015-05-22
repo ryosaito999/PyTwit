0c394cc36e36d56dd70c609952bfe1bc9a7da9f8
ryosaito420 <ryocsaito@gmail.com>
2015-05-22 14:11:38 -0700

    setup basic verification

--- echo.py
+++ echo.py
@@ -6,7 +6,6 @@ import sys
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 8888 # Arbitrary non-privileged port
 
-userlist = []
 
 class User:
 	def __init__(self, uname, pwd):
@@ -21,10 +20,20 @@ class User:
 		else:
 			return False
 
+
+
+
+userlist = []
 user1 = User("gintoki", "ichigo")
 user2 = User("kagura", "sadaharu")
 user3 = User("shinpachi", "glasses")
 user4 = User("fruitspunchsamurai", "otae")
+userlist.append(user1);
+userlist.append(user2);
+userlist.append(user3);
+userlist.append(user4);
+
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
@@ -45,8 +54,9 @@ print 'Connected with ' + addr[0] + ':' + str(addr[1])
 userTemp = conn.recv(1024)
 pwdTemp = conn.recv(1024)
 
-if User.userVerify(userTemp, pwdTemp) is True:
-	print "welcome back!"
+for user in userlist:
+	if user.userVerify(userTemp,pwdTemp) is True:
+		print "welcome back!"
 
 conn.close()
 s.close()
