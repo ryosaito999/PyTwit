3e15d36c9737489f87f55fa86372a2922fffeb25
ryosaito420 <ryocsaito@gmail.com>
2015-05-29 12:59:56 -0700

    finally have login working!

--- client.py
+++ client.py
@@ -2,7 +2,12 @@ import socket   #for sockets
 import sys  #for exit
 from check import ip_checksum
 import select
- 
+import time
+import os
+
+
+userOK = False
+
 #create an INET, STREAMing socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,13 +29,31 @@ except socket.gaierror:
 #Connect to remote server
 s.connect((remote_ip , port))
 print 'Welcome to pyTwit! Enter your username and password.\n' 
-uname = str(raw_input( 'username: ' ))
-pwd = str(raw_input( 'password: '))
 
-s.sendall(uname)
-s.sendall(pwd)
+while userOK == False :  
+	uname = str(raw_input( 'username: ' ))
+	pwd = str(raw_input( 'password: '))
 
-verifyMSG = s.recv(4096)
-print verifyMSG
+	s.send(uname)
+	time.sleep(2)
+	s.send(pwd)
+	time.sleep(1)
 
+	vMsg = s.recv(1024)
+	time.sleep(1)
+
+
+	if vMsg == str(1):
+		userOK = True
+		os.system('clear')
+		print 'Welcome back, ' + uname + '!\n'
+
+	else:
+		print 'Incorrect username/password! Please reenter.\n'
+
+
+#-----------------------------------------------------------------------------------------------
+#Start main useraccnt here!
+msgNum = s.recv(1024)
+print 'You have ' + msgNum + ' unread messages.\n'
 
--- echo.py
+++ echo.py
@@ -11,6 +11,7 @@ class User:
 	def __init__(self, u, p):
 		self.uname = u;
 		self.pwd =  p;
+		self.msgList = []
 
 
 	def userVerify(self, u, p):
@@ -20,30 +21,44 @@ class User:
 		else:
 			return False
 
+	def getMsgAmnt(self):
+		return len(self.msgList)
+
+	def addMessage(self, submittedMsg):
+		return None
+
+	def logOut(self):
+		return None
 
 #Define functions here!
 
 def checkUserList(ulist, userTemp, pwdTemp): 
 	for user in ulist:
 		if user.userVerify(userTemp , pwdTemp) is True:
-			return "Welcome back! " + user.uname + ".\n"
+			return user
 
-	return "User/Password is incorrect! Re-Enter information. \n"
+	return None
 
-#Declare User List Here!
-userlist = []
-user1 = User("gintoki", "ichigo")
-user2 = User("kagura", "sadaharu")
-user3 = User("shinpachi", "glasses")
-user4 = User("fruitspunchsamurai", "otae")
-userlist.append(user1);
-userlist.append(user2);
-userlist.append(user3);
-userlist.append(user4);
+def userNameDeclare():
+	#Declare User List Here!
+	userlist = []
+	user1 = User("gintoki", "ichigo")
+	user2 = User("kagura", "sadaharu")
+	user3 = User("shinpachi", "glasses")
+	user4 = User("fruitspunchsamurai", "otae")
+	userlist.append(user1);
+	userlist.append(user2);
+	userlist.append(user3);
+	userlist.append(user4);
+	return userlist
+
+def sendUserMsgNum(curUser):
+	return curUser.getMsgAmnt()
+
 
 
 #Main Code
-
+userlist = userNameDeclare()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
  
@@ -60,13 +75,41 @@ print 'Socket now listening'
 
 conn, addr = s.accept()
 print 'Connected with ' + addr[0] + ':' + str(addr[1])     
-userTemp = conn.recv(1024)
-pwdTemp = conn.recv(1024)
+#--------------------------------------------------------------------------------------------------------------------------------
 
-msg = checkUserList(userlist,userTemp, pwdTemp) 
-s.sendall(msg)
-#conn.close()
-#s.close()
+userVerify = False
+
+while userVerify is False:
+	
+	userTemp = conn.recv(1024)
+	print userTemp
+
+	pwdTemp = conn.recv(1024)
+	print pwdTemp
+
+	curUser = checkUserList(userlist,userTemp, pwdTemp) 
+	if curUser:
+		msg = str(1)
+		userVerify = True
+
+	else:
+		msg = str(0)
+
+
+	print 'Send: '+ msg
+	conn.send(msg)
+	time.sleep(1)
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+print 'waiting on Menu selection'
+
+conn.send(str(sendUserMsgNum(curUser)))
+
+
+conn.close()
+s.close()
 
 
 
